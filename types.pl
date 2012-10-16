957c28ddcbf8f95e276ea979b59e03d25358507a
Maël Nison <nison.mael@gmail.com>
2012-10-16 12:04:44 -0400

    Types ratios & pokemons types databases

---
+++ types.pl
@@ -0,0 +1,125 @@
+ratio(normal, rock, 0.5).
+ratio(normal, ghost, 0.0).
+ratio(normal, steel, 0.5).
+
+ratio(fire, fire, 0.5).
+ratio(fire, water, 0.5).
+ratio(fire, grass, 2.0).
+ratio(fire, ice, 2.0).
+ratio(fire, bug, 2.0).
+ratio(fire, rock, 0.5).
+ratio(fire, dragon, 0.5).
+ratio(fire, steel, 2.0).
+
+ratio(water, fire, 2.0).
+ratio(water, water, 0.5).
+ratio(water, grass, 0.5).
+ratio(water, ground, 2.0).
+ratio(water, rock, 2.0).
+ratio(water, dragon, 0.5).
+
+ratio(electric, water, 2.0).
+ratio(electric, electric, 0.5).
+ratio(electric, ground, 0.0).
+ratio(electric, flying, 2.0).
+ratio(electric, dragon, 0.5).
+
+ratio(grass, fire, 0.5).
+ratio(grass, water, 2.0).
+ratio(grass, grass, 0.5).
+ratio(grass, poison, 0.5).
+ratio(grass, ground, 2.0).
+ratio(grass, flying, 0.5).
+ratio(grass, bug, 0.5).
+ratio(grass, rock, 2.0).
+ratio(grass, dragon, 0.5).
+ratio(grass, steel, 0.5).
+
+ratio(ice, fire, 0.5).
+ratio(ice, water, 0.5).
+ratio(ice, grass, 2.0).
+ratio(ice, ice, 0.5).
+ratio(ice, ground, 2.0).
+ratio(ice, flying, 2.0).
+ratio(ice, dragon, 2.0).
+ratio(ice, steel, 0.5).
+
+ratio(fighting, normal, 2.0).
+ratio(fighting, ice, 2.0).
+ratio(fighting, poison, 0.5).
+ratio(fighting, flying, 0.5).
+ratio(fighting, psychic, 0.5).
+ratio(fighting, bug, 0.5).
+ratio(fighting, rock, 2.0).
+ratio(fighting, ghost, 0.0).
+ratio(fighting, darkness, 2.0).
+ratio(fighting, steel, 2.0).
+
+ratio(poison, grass, 2.0).
+ratio(poison, poison, 0.5).
+ratio(poison, ground, 0.5).
+ratio(poison, rock, 0.5).
+ratio(poison, ghost, 0.5).
+ratio(poison, steel, 0.0).
+
+ratio(ground, fire, 2.0).
+ratio(ground, eletric, 2.0).
+ratio(ground, grass, 0.5).
+ratio(ground, poison, 2.0).
+ratio(ground, flying, 0.0).
+ratio(ground, bug, 0.5).
+ratio(ground, rock, 2.0).
+ratio(ground, steel, 2.0).
+
+ratio(flying, electric, 0.5).
+ratio(flying, grass, 2.0).
+ratio(flying, fighting, 2.0).
+ratio(flying, bug, 2.0).
+ratio(flying, rock, 0.5).
+ratio(flying, steel, 0.5).
+
+ratio(psychic, fighting, 2.0).
+ratio(psychic, poison, 2.0).
+ratio(psychic, psychic, 0.5).
+ratio(psychic, dark, 0.0).
+ratio(psychic, steel, 0.5).
+
+ratio(bug, fire, 0.5).
+ratio(bug, grass, 2.0).
+ratio(bug, fighting, 0.5).
+ratio(bug, poison, 0.5).
+ratio(bug, fly, 0.5).
+ratio(bug, psychic, 2.0).
+ratio(bug, ghost, 0.5).
+ratio(bug, dark, 2.0).
+ratio(bug, steel, 0.5).
+
+ratio(rock, fire, 2.0).
+ratio(rock, ice, 2.0).
+ratio(rock, fighting, 0.5).
+ratio(rock, ground, 0.5).
+ratio(rock, fly, 2.0).
+ratio(rock, bug, 2.0).
+ratio(rock, steel, 0.5).
+
+ratio(ghost, normal, 0.0).
+ratio(ghost, psychic, 2.0).
+ratio(ghost, ghost, 2.0).
+ratio(ghost, dark, 0.5).
+ratio(ghost, steel, 0.5).
+
+ratio(dragon, dragon, 2.0).
+ratio(dragon, steel, 0.5).
+
+ratio(dark, fighting, 0.5).
+ratio(dark, psychic, 2.0).
+ratio(dark, ghost, 2.0).
+ratio(dark, dark, 0.5).
+ratio(dark, steel, 0.5).
+
+ratio(steel, fire, 0.5).
+ratio(steel, water, 0.5).
+ratio(steel, electric, 0.5).
+ratio(steel, ice, 2.0).
+ratio(steel, rock, 2.0).
+ratio(steel, steel, 0.5).
