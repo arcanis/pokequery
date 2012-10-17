139a4206d6ea9a5254d5e568e9bc5bc6806a4b95
Maël Nison <nison.mael@gmail.com>
2012-10-17 00:28:30 -0400

    Adds evolutionary line predicates

--- src/pklib.pl
+++ src/pklib.pl
@@ -1,14 +1,13 @@
-pklib_foldList(Result, _, [], Result).
-pklib_foldList(Start, Callable, [Head | Tail], Result) :- call(Callable, Start, Head, Temp), pklib_foldList(Temp, Callable, Tail, Result).
-
-pklib_plus(A, B, Result) :- Result is A + B.
-pklib_sumList(List, Result) :- pklib_foldList(0, pklib_plus, List, Result).
-
-pklib_product(A, B, Result) :- Result is A * B.
-pklib_productList(List, Result) :- pklib_foldList(1, pklib_plus, List, Result).
-
 pklib_ratio([], _, 1).
 pklib_ratio(_, [], 1).
 pklib_ratio([Attack | Tail], Defense, Result) :- pklib_ratio(Attack, Defense, Temp1), pklib_ratio(Tail, Defense, Temp2), Result is Temp1 * Temp2.
 pklib_ratio(Attack, [Defense | Tail], Result) :- pklib_ratio(Attack, Defense, Temp1), pklib_ratio(Attack, Tail, Temp2), Result is Temp1 * Temp2.
 pklib_ratio(Attack, Defense, Result) :- ratio(Attack, Defense, Result).
+
+pklib_evolutionary_line_root(Species, Result) :- pkdb_evolution(Preevolution, Species), pklib_evolutionary_line_root(Preevolution, Result).
+pklib_evolutionary_line_root(Species, Species) :- \+ pkdb_evolution(_, Species), !.
+
+pklib_evolutionary_line(Species, Result) :- pklib_evolutionary_line_root(Species, Root), pklib_evolutions(Root, Evolutions), Result = [Root | Evolutions].
+
+pklib_evolutions(Species, Result) :- pkdb_evolution(Species, Evolution), pklib_evolutions(Evolution, Rest), Result = [Evolution | Rest].
+pklib_evolutions(Species, []) :- \+ pkdb_evolution(Species, _).
