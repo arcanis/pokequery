8a027400cfba293b59659c4b0cf224d255b48a62
Maël Nison <nison.mael@gmail.com>
2012-10-18 19:51:14 -0400

    Removes pklib_type_interactions which isn't useful

--- src/pklib.pl
+++ src/pklib.pl
@@ -1,4 +1,4 @@
-pklib_type_ratio([Attack | []], [Defense | []], Result) :-
+pklib_type_ratio([Attack], [Defense], Result) :-
     pkdb_type_ratio(Attack, Defense, Result).
 pklib_type_ratio([Attack | Tail], Defense, Result) :- pkdb_type(Attack),
     pklib_type_ratio(Attack, Defense, Temp1),
@@ -10,14 +10,6 @@ pklib_type_ratio(Attack, [Defense | Tail], Result) :- pkdb_type(Defense),
     pklib_type_ratio(Attack, Tail, Temp2),
     \+ member(Defense, Tail),
     Result is Temp1 * Temp2.
-
-pklib_type_interactions(Types, X0_00, X0_25, X0_50, X1_00, X2_00, X4_00) :-
-    findall(pkdb_type(Type), pklib_type_ratio(Types, Type, 0.00), X0_00),
-    findall(pkdb_type(Type), pklib_type_ratio(Types, Type, 0.25), X0_25),
-    findall(pkdb_type(Type), pklib_type_ratio(Types, Type, 0.50), X0_50),
-    findall(pkdb_type(Type), pklib_type_ratio(Types, Type, 1.00), X1_00),
-    findall(pkdb_type(Type), pklib_type_ratio(Types, Type, 2.00), X2_00),
-    findall(pkdb_type(Type), pklib_type_ratio(Types, Type, 4.00), X4_00).
 
 pklib_evolutionary_line_root(Species, Result) :- pkdb_species_evolution(Preevolution, Species), pklib_evolutionary_line_root(Preevolution, Result).
 pklib_evolutionary_line_root(Species, Species) :- \+ pkdb_species_evolution(_, Species).
