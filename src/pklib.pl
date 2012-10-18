9dddb5708dd8264e2590f3c7064433e9faa5c318
Maël Nison <nison.mael@gmail.com>
2012-10-18 19:45:15 -0400

    Fixes issues, pklib_type_ratio() takes only list parameters

--- src/pklib.pl
+++ src/pklib.pl
@@ -1,18 +1,16 @@
-pklib_type_ratio([], _, 1.0).
-pklib_type_ratio(_, [], 1.0).
-pklib_type_ratio([Attack | Tail], Defense, Result) :- pkdb_type(Attack), \+ member(Attack, Tail),
+pklib_type_ratio([Attack | []], [Defense | []], Result) :-
+    pkdb_type_ratio(Attack, Defense, Result).
+pklib_type_ratio([Attack | Tail], Defense, Result) :- pkdb_type(Attack),
     pklib_type_ratio(Attack, Defense, Temp1),
     pklib_type_ratio(Tail, Defense, Temp2),
+    \+ member(Attack, Tail),
     Result is Temp1 * Temp2.
-pklib_type_ratio(Attack, [Defense | Tail], Result) :- pkdb_type(Defense), \+ member(Defense, Tail),
+pklib_type_ratio(Attack, [Defense | Tail], Result) :- pkdb_type(Defense),
     pklib_type_ratio(Attack, Defense, Temp1),
     pklib_type_ratio(Attack, Tail, Temp2),
+    \+ member(Defense, Tail),
     Result is Temp1 * Temp2.
-pklib_type_ratio(Attack, Defense, Result) :-
-    pkdb_type_ratio(Attack, Defense, Result).
 
-pklib_type_interactions([], [], [], []).
-pklib_type_interactions([], [], [], []).
 pklib_type_interactions(Types, X0_00, X0_25, X0_50, X1_00, X2_00, X4_00) :-
     findall(pkdb_type(Type), pklib_type_ratio(Types, Type, 0.00), X0_00),
     findall(pkdb_type(Type), pklib_type_ratio(Types, Type, 0.25), X0_25),
@@ -22,7 +20,7 @@ pklib_type_interactions(Types, X0_00, X0_25, X0_50, X1_00, X2_00, X4_00) :-
     findall(pkdb_type(Type), pklib_type_ratio(Types, Type, 4.00), X4_00).
 
 pklib_evolutionary_line_root(Species, Result) :- pkdb_species_evolution(Preevolution, Species), pklib_evolutionary_line_root(Preevolution, Result).
-pklib_evolutionary_line_root(Species, Species) :- \+ pkdb_species_evolution(_, Species), !.
+pklib_evolutionary_line_root(Species, Species) :- \+ pkdb_species_evolution(_, Species).
 
 pklib_evolutionary_line(Species, Result) :- pklib_evolutionary_line_root(Species, Root), pklib_evolutions(Root, Evolutions), Result = [Root | Evolutions].
 
