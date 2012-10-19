9014778b3a8469f9be671e0184b324760ee00773
Maël Nison <nison.mael@gmail.com>
2012-10-18 22:07:29 -0400

    Removes useless predicates

--- src/pklib.pl
+++ src/pklib.pl
@@ -1,4 +1,4 @@
-pklib_max_type_list_length(2).
+:- use_module(library(clpq)).
 
 pklib_type_ratio(Attack, Defense, Result) :-
     pklib_type_ratio_unroll_attack(Attack, Defense, Result).
@@ -10,7 +10,6 @@ pklib_type_ratio_unroll_attack([Attack | Tail], Defense, Result) :-
     pktool_limit_length([Attack | Tail], 2),
     pklib_type_ratio_unroll_defense(Attack, Defense, Temp1),
     pklib_type_ratio_unroll_attack(Tail, Defense, Temp2),
-    \+ member(Attack, Tail),
     Result is Temp1 * Temp2.
 
 pklib_type_ratio_unroll_defense(_, [], 1.0).
@@ -18,7 +17,6 @@ pklib_type_ratio_unroll_defense(Attack, [Defense | Tail], Result) :-
     pkdb_type(Defense),
     pktool_not_member(Defense, Tail),
     pktool_limit_length([Attack | Tail], 2),
-    pklib_max_type_list_length(MaxTypeListLength), length(Tail, TypeListLength), MaxTypeListLength >= TypeListLength,
     pkdb_type_ratio(Attack, Defense, Temp1),
     pklib_type_ratio_unroll_defense(Attack, Tail, Temp2),
     Result is Temp1 * Temp2.
